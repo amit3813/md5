d534d8083f902d15696a27e67783a0173355116f
Amit Tachan <amit.tachan@gmail.com>
2021-10-24 21:44:14 +0300

    first commit

---
+++ Tochna/cyber/12y/atozHashCodePrep.py
@@ -0,0 +1,20 @@
+import string
+
+ltrs = string.ascii_lowercase
+
+for i in ltrs:
+    for j in ltrs:
+        for k in ltrs:
+            for x in ltrs:
+                for y in ltrs:
+                    for z in ltrs:
+                        str = i+j+k+x+y+z
+                        print(str)
+
+                            
+
+
+
+
+
+
